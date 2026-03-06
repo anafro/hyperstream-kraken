12bac44c725288c1e1b1e7ddf62d497f20b04c51
Anatoly Frolov (anafro) <anatolyfroloff@gmail.com>
2026-03-06 11:58:49 +1000

    chore(storage): make models convertible to dict

--- src/hyperstreamkraken/models/base.py
+++ src/hyperstreamkraken/models/base.py
@@ -1,5 +1,15 @@
+from typing import Any
+from sqlalchemy import inspect
 from sqlalchemy.orm import DeclarativeBase
+import json
 
 
 class Base(DeclarativeBase):
-    pass
+    def to_dict(self) -> dict[str, Any]:
+        return {
+            column.key: getattr(self, column.key)
+            for column in inspect(self).mapper.column_attrs
+        }
+
+    def to_json(self) -> str:
+        return json.dumps(self.to_dict())
